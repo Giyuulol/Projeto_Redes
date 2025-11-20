20595f13263d07fdbcee989e501d9b260478057b
Diego Braga <164006890+XicoBr@users.noreply.github.com>
2025-11-20 16:34:45 -0300

    Add files via upload

---
+++ proj_wireshark/tcp_client.py
@@ -0,0 +1,26 @@
+from socket import *
+
+serverName = 'localhost'
+
+mensagem = "gustavo"
+
+serverPort = 3000
+
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+clientSocket.connect((serverName, serverPort))
+
+
+#a mensagem deve estar em bytes antes de ser enviada ao buffer de transmissão
+clientSocket.send(mensagem.encode())
+
+
+#recebe a resposta do servidor
+clientSocket.recv(1024)
+
+#devemos converter a mensagem de volta para string antes de imprimí-la
+print('Resposta:' , mensagem)
+
+#fecha a conexão
+clientSocket.close()
